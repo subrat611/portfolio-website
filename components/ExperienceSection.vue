71f0d0f8c43e141fa34892d71e5bde5942bb3817
subrat <subratj611@gmail.com>
2023-03-16 19:23:13 +0530

    experience section created

---
+++ components/ExperienceSection.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "ExperienceSection",
+};
+</script>
+
+<template>
+  <div class="experience-container">
+    <div class="experience-wrapper">
+      <h1 class="section-title">Experience</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.experience-container {
+  padding: 0 10%;
+
+  .experience-wrapper {
+    .section-title {
+      font-size: 2.5rem;
+      font-family: "Manrope", sans-serif;
+      font-weight: 700;
+      text-align: center;
+    }
+  }
+}
+</style>
