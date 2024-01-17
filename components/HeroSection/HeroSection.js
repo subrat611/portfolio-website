fb80fdfb30cc1487392cb0499bf911fc9d04ef44
Subrat kumar jena <subratj611@gmail.com>
2024-01-17 18:20:48 +0530

    ui: hero section re-design

--- components/HeroSection/HeroSection.js
+++ components/HeroSection/HeroSection.js
@@ -1,7 +1,13 @@
 import { useContext } from "react";
 import Image from "next/image";
 
-import { DocIcon, GithubIcon, LinkedinIcon, TwitterIcon } from "@/utils/icons";
+import {
+  DocIcon,
+  GithubIcon,
+  IconBuddyLeetCodeIcon,
+  LinkedinIcon,
+  TwitterIcon,
+} from "@/utils/icons";
 
 import { ThemeContext } from "@/context/ThemeModeContext";
 
@@ -9,108 +15,63 @@ const HeroSection = () => {
   const { themeMode } = useContext(ThemeContext);
 
   return (
-    <section className="w-11/12 max-w-[780px] mx-auto px-4 md:px-0">
-      <h1 className="text-4xl text-slate-50 font-bold w-[220px] sm:w-full">
-        Subrat Kumar Jena
-      </h1>
-      <p className="text-base text-gray-300 mt-1">
-        Fullstack JavaScript Developer from 🇮🇳
-      </p>
-      <div className="flex flex-col sm:flex-row mt-3">
-        <div className="ml-1 mt-2 sm:ml-0">
-          <div className="my-3">
-            <a
-              className={`cursor-pointer flex items-center ${themeMode} group`}
-              href="https://github.com/subrat611"
-              target="_blank"
-            >
-              <GithubIcon />
-              <span className="text-gray-400 ml-2 font-medium group-hover:text-gray-300">
-                GitHub Profile
-              </span>
-            </a>
-          </div>
-          <div className="my-3">
-            <a
-              className={`cursor-pointer flex items-center ${themeMode} group`}
-              href="https://www.linkedin.com/in/subratkumarjena/"
-              target="_blank"
-            >
-              <LinkedinIcon />
-              <span className="text-gray-400 ml-2 font-medium group-hover:text-gray-300">
-                Linkedin Profile
-              </span>
-            </a>
-          </div>
-          <div className="my-3">
-            <a
-              className={`cursor-pointer flex items-center ${themeMode} group`}
-              href="https://twitter.com/nikhil88950"
-              target="_blank"
-            >
-              <TwitterIcon />
-              <span className="text-gray-400 ml-2 font-medium group-hover:text-gray-300">
-                Twitter Profile
-              </span>
-            </a>
-          </div>
-          {/* <div className="my-3">
-            <a
-              className={`cursor-pointer flex items-center ${themeMode} group`}
-            >
-              <DocIcon />
-              <span className="text-gray-400 ml-2 font-medium group-hover:text-gray-300">
-                Resume
-              </span>
-            </a>
-          </div> */}
-        </div>
-      </div>
-
-      <div className="py-8">
-        <p className="text-slate-50 font-semibold text-2xl">About me</p>
-        <p className="text-gray-300 text-lg mt-2">
-          <span className="text-cyan-300">
-            🚀 Passionate Frontend Developer | Open Source Enthusiast{" "}
-          </span>
-          <br />
-          <span>
-            Hello there! I'm Subrat Kumar Jena, a final year B.Tech student with
-            a keen passion for front-end development and a touch of backend
-            expertise. I thrive in the world of web technologies, specializing
-            in crafting seamless user experiences and robust applications.
-          </span>
-          <br />
-          <span className="mt-3">
-            💻 Technical Expertise: <br />
-            Languages:{" "}
-            <span className="text-cyan-300">
-              C++, JavaScript, TypeScript, and Java.
-            </span>
-            <br />
-            Front-end technologies:{" "}
-            <span className="text-cyan-300">
-              React.js, Vue.js, HTML, CSS, SCSS, Material UI, Next.js, Nuxt.js,
-              and Redux Toolkit
-            </span>
-            <br />
-            Backend technologies:{" "}
-            <span className="text-cyan-300">Node.js, MongoDB.</span> <br />
-            I'm well-versed in version control systems like Git and platforms
-            like GitHub and Bitbucket. Additionally, I also have some
-            familiarity with Docker and Firebase.
-          </span>
+    <section className="h-full min-h-[80vh] flex flex-col pt-10 md:pt-20">
+      <div>
+        <p className="capitalize text-xl text-green-300 font-medium">
+          frontend developer
         </p>
-        {/* <p className="text-gray-300 mt-3 hidden">
-          With 2 impactful 6-month internships under my belt, I've honed my
-          skills in real-world scenarios, working extensively with the
-          technologies mentioned above. During my internships, I not only
-          developed robust applications but also collaborated with
-          cross-functional teams, ensuring seamless project delivery. Outside of
-          work, I'm proud to have contributed to open-source initiatives like
-          HackOctoberFest and GSSOC 2023, where I collaborated with fellow
-          developers on meaningful projects.
-        </p> */}
+        <label className="relative inline-flex items-center cursor-pointer mt-2">
+          <input
+            type="checkbox"
+            value=""
+            className="sr-only peer"
+            defaultChecked
+          />
+          <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-green-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-500"></div>
+          <span className="ms-3 text-sm font-medium text-gray-200">
+            Available for work
+          </span>
+        </label>
+      </div>
+      <div className="mt-5 flex items-center space-x-3">
+        <a
+          href="https://github.com/subrat611/"
+          target="_blank"
+          className="text-gray-400 hover:text-gray-50"
+        >
+          <GithubIcon />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/subratkumarjena/"
+          target="_blank"
+          className="text-gray-400 hover:text-gray-50"
+        >
+          <LinkedinIcon />
+        </a>
+        <a
+          href="https://leetcode.com/s1jena/"
+          target="_blank"
+          className="text-gray-400 hover:text-gray-50"
+        >
+          <IconBuddyLeetCodeIcon />
+        </a>
+        <a
+          href="https://twitter.com/nikhil88950"
+          target="_blank"
+          className="text-gray-400 hover:text-gray-50"
+        >
+          <TwitterIcon />
+        </a>
+      </div>
+      <div className="my-4">
+        <p className="text-2xl text-gray-400 leading-8 md:leading-10 md:text-3xl">
+          Hello there! I'm{" "}
+          <span className="text-gray-200 font-medium">Subrat Kumar Jena</span>,
+          a final year B.Tech student with a keen passion for frontend
+          development and a touch of backend expertise. I thrive in the world of
+          web technologies, specializing in crafting seamless user experiences
+          and robust applications.
+        </p>
       </div>
     </section>
   );
