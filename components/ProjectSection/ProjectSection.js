0b07b77a1aedb43157f02a02c7590aa3914ab8c7
Subrat kumar jena <subratj611@gmail.com>
2024-01-19 12:35:02 +0530

    add: project skills

--- components/ProjectSection/ProjectSection.js
+++ components/ProjectSection/ProjectSection.js
@@ -1,55 +1,77 @@
+import pinnedProjects from "@/utils/projects";
+import Link from "next/link";
+
 const ProjectSection = () => {
   return (
-    <section>
+    <section className="pb-10 flex flex-col items-start">
       <p className="text-2xl sm:text-3xl text-gray-200 font-semibold">
         Pinned Projects
       </p>
       <div className="py-5">
-        {[...new Array(1)].map((_, i) => (
-          <div key={i}>
-            <div className="border-b border-gray-600" />
-            <div className="py-7 flex items-start flex-col-reverse md:flex-row">
-              <div className="pr-4">
-                <p className="text-xl text-gray-400 font-semibold py-2">
-                  NxDraw
-                </p>
-                <p className="text-2xl text-gray-200 pb-5">
-                  An online whiteboard application built with Next.js.
-                </p>
-                <p>skills</p>
-                <div className="flex items-center space-x-2">
-                  <a
-                    href="https://github.com/subrat611/NxDraw"
-                    target="_blank"
-                    className="bg-transparent border border-gray-700 p-2 text-gray-200 rounded-md hover:bg-gray-600/30"
-                    aria-label="link to project repository"
-                  >
-                    View project
-                  </a>
-                  <a
-                    href="https://nxdraw.vercel.app/"
-                    className="bg-transparent border border-gray-700 p-2 text-gray-200 rounded-md hover:bg-gray-600/30"
-                    aria-label="link to live site"
-                  >
-                    Live site
-                  </a>
+        {pinnedProjects.map(
+          (
+            { project_name, description, tech, gitLink, liveLink, video },
+            i
+          ) => (
+            <div key={i}>
+              <div className="border-b border-gray-600" />
+              <div className="py-7 flex items-start flex-col-reverse md:flex-row">
+                <div className="pr-4">
+                  <p className="text-xl text-gray-400 font-semibold py-2">
+                    {project_name}
+                  </p>
+                  <p className="text-2xl text-gray-200 pb-5">{description}</p>
+                  <div className="flex flex-wrap items-center mb-2">
+                    {tech.map(({ name, icon }, i) => (
+                      <div
+                        key={`${name}-${i}`}
+                        className="text-gray-200 font-medium flex items-center px-[10px] py-[5px] border border-gray-700 mr-2 mb-3 rounded-md text-sm"
+                      >
+                        {icon}
+                        <span className="ml-2">{name}</span>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <a
+                      href={gitLink}
+                      target="_blank"
+                      className="bg-transparent border border-gray-700 p-2 text-gray-200 rounded-md hover:bg-gray-600/30"
+                      aria-label="link to project repository"
+                    >
+                      View project
+                    </a>
+                    <a
+                      href={liveLink}
+                      className="bg-transparent border border-gray-700 p-2 text-gray-200 rounded-md hover:bg-gray-600/30"
+                      aria-label="link to live site"
+                    >
+                      Live site
+                    </a>
+                  </div>
                 </div>
-              </div>
 
-              {/* video player */}
-              <video
-                className="w-[360px] h-full rounded-lg bg-transparent"
-                src="https://res.cloudinary.com/dr1dgktil/video/upload/f_auto:video,q_auto/v1/portfolio/q5uy8i7csf4vsenudktg"
-                controls
-                loop
-                autoPlay={false}
-                playsInline
-              ></video>
+                {/* video player */}
+                <video
+                  className="w-[360px] h-full rounded-lg bg-transparent"
+                  src={video}
+                  controls
+                  loop
+                  autoPlay={false}
+                  playsInline
+                ></video>
+              </div>
             </div>
-            {i === 1 && <div className="border-b border-gray-600" />}
-          </div>
-        ))}
+          )
+        )}
       </div>
+      <Link
+        type="button"
+        href="/projects"
+        className="border border-gray-700 rounded-md bg-gray-800/40 text-gray-100 py-2 px-3 self-center"
+      >
+        View all projects
+      </Link>
     </section>
   );
 };
--- utils/projects.js
+++ utils/projects.js
@@ -1,20 +1,56 @@
-const projects = [
+import { JavaScriptIcon, NextJsIcon, TailwindcssIcon } from "./icons";
+
+const pinnedProjects = [
   {
     project_name: "NxDraw",
     description: "An online whiteboard application built with Next.js.",
     tech: [
       {
-        name: "Next.js",
-        clr: "blue",
+        name: "JavaScript",
+        icon: <JavaScriptIcon />,
       },
       {
-        name: "tailwindcss",
-        clr: "sky",
+        name: "Next.js",
+        icon: <NextJsIcon />,
+      },
+      {
+        name: "Tailwind CSS",
+        icon: <TailwindcssIcon />,
+      },
+      {
+        name: "HTML 5 Canvas",
       },
     ],
     gitLink: "https://github.com/subrat611/NxDraw",
     liveLink: "https://nxdraw.vercel.app/",
+    video:
+      "https://res.cloudinary.com/dr1dgktil/video/upload/f_auto:video,q_auto/v1/portfolio/q5uy8i7csf4vsenudktg",
+  },
+  {
+    project_name: "NxDraw",
+    description: "An online whiteboard application built with Next.js.",
+    tech: [
+      {
+        name: "JavaScript",
+        icon: <JavaScriptIcon />,
+      },
+      {
+        name: "Next.js",
+        icon: <NextJsIcon />,
+      },
+      {
+        name: "Tailwind CSS",
+        icon: <TailwindcssIcon />,
+      },
+      {
+        name: "HTML 5 Canvas",
+      },
+    ],
+    gitLink: "https://github.com/subrat611/NxDraw",
+    liveLink: "https://nxdraw.vercel.app/",
+    video:
+      "https://res.cloudinary.com/dr1dgktil/video/upload/f_auto:video,q_auto/v1/portfolio/q5uy8i7csf4vsenudktg",
   },
 ];
 
-export default projects;
+export default pinnedProjects;
